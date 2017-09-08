b30b9a1cd06e8d5011753c620a91ad8348fb9c23
杜鹏 <dupeng@dupengdeimac.local>
2017-09-08 17:08:58 +0800

    ／／／

--- gitTest/gitTest/ViewController.swift
+++ gitTest/gitTest/ViewController.swift
@@ -6,12 +6,20 @@
 //  Copyright © 2017年 杜鹏. All rights reserved.
 //
 
+
 import UIKit
+import MediaPlayer
+//import AliyunPlayerSDK
+import SystemConfiguration.CaptiveNetwork
+import MobileCoreServices
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+      let wifi = getWifiInfo()
+        print(wifi.0)
         //hello
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -21,6 +29,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var test: UIButton!
+    @IBAction func btn(_ sender: Any) {
+        let url = URL.init(string: "app-Prefs:root=WIFI")//app-Prefs:root=WIFI
+        
+//        http://www.jianshu.com/p/4c64ea08012a
+        
+        if UIApplication.shared.canOpenURL(url!){
+//             UIApplication.shared.canOpenURL(url!)
+            UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+        }
+        
+        
+    }
+    
+    
+    //获取 WiFi 信息
+    func getWifiInfo() -> (ssid: String, mac: String) {
+        if let cfas: NSArray = CNCopySupportedInterfaces() {
+            for cfa in cfas {
+                if let dict:NSDictionary = CFBridgingRetain(
+                    CNCopyCurrentNetworkInfo(cfa as! CFString)
+                    ) as? NSDictionary {
+                    
+                    if let ssid = dict["SSID"] as? String,
+                        let bssid = dict["BSSID"] as? String {
+                        return (ssid, bssid)
+                    }
+                }
+            }
+        }
+        return ("未知", "未知")
+    }
+    
+  
+    
 
 }
 
---
+++ gitTest/gitTest/gitTest-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//#import <AliyunPlayerSDK/AliyunPlayerSDK.h>
+//http://www.cocoachina.com/ios/20161019/17791.html
